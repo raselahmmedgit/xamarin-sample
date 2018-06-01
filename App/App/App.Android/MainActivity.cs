3977680a1431fd1f246dd1513bf2bbc61f839e0d
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2018-06-01 16:56:10 +0600

    20180601

---
+++ App/App/App.Android/MainActivity.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+using Java.Lang;
+using String = System.String;
+using NotificationCompat = Android.Support.V4.App.NotificationCompat;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
+using Android.Gms.Common;
+using Android.Util;
+
+namespace App.Droid
+{
+    [Activity(Label = "Push Notification", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        // Unique ID for our notification: 
+        private static readonly int ButtonClickNotificationId = 1000;
+
+        // Number of times the button is tapped (starts with first tap):
+        private int count = 1;
+
+        TextView textViewRemotePushNotification;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.btnLocalPushNotification);
+
+            button.Click += ButtonOnClick;
+
+            IsPlayServicesAvailable();
+        }
+
+        #region Local
+        // Handler for button click events.
+        private void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            // Pass the current button press count value to the next activity:
+            Bundle valuesForActivity = new Bundle();
+            valuesForActivity.PutInt("count", count);
+
+            // When the user clicks the notification, SecondActivity will start up.
+            Intent resultIntent = new Intent(this, typeof(SecondActivity));
+
+            // Pass some values to SecondActivity:
+            resultIntent.PutExtras(valuesForActivity);
+
+            // Construct a back stack for cross-task navigation:
+            TaskStackBuilder stackBuilder = TaskStackBuilder.Create(this);
+            stackBuilder.AddParentStack(Class.FromType(typeof(SecondActivity)));
+            stackBuilder.AddNextIntent(resultIntent);
+
+            // Create the PendingIntent with the back stack:            
+            PendingIntent resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+
+            // Build the notification:
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .SetAutoCancel(true)                    // Dismiss the notification from the notification area when the user clicks on it
+                .SetContentIntent(resultPendingIntent)  // Start up this activity when the user clicks the intent.
+                .SetContentTitle("Button Clicked")      // Set the title
+                .SetNumber(count)                       // Display the count in the Content Info
+                .SetSmallIcon(Resource.Drawable.Icon) // This is the icon to display
+                .SetContentText(String.Format("The button has been clicked {0} times.", count)); // the message to display.
+
+            // Finally, publish the notification:
+            NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+            notificationManager.Notify(ButtonClickNotificationId, builder.Build());
+
+            // Increment the button press count:
+            count++;
+        }
+
+        #endregion
+
+        #region Remote
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    textViewRemotePushNotification.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    textViewRemotePushNotification.Text = "Sorry, this device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                textViewRemotePushNotification.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+        #endregion
+    }
+}
+
+
---
+++ App/App/App.Android/SecondActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App.Droid
+{
+    [Activity(Label = "Push Notification", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Get the count value passed to us from MainActivity:
+            int count = Intent.Extras.GetInt("count", -1);
+
+            // No count was passed? Then just return.
+            if (count <= 0)
+            {
+                return;
+            }
+
+            // Display the count sent from the first activity:
+            SetContentView(Resource.Layout.Second);
+            TextView textView = FindViewById<TextView>(Resource.Id.textViewLocalPushNotification);
+            textView.Text = String.Format("You clicked the button {0} times in the previous activity.", count);
+        }
+    }
+}
